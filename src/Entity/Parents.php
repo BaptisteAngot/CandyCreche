d18abe8f663ce7d59668d7b24350820738b658f7
Nicolas Dufresne <nicoladufresne@gmail.com>
2019-03-28 16:23:42 +0100

    Déplacement du css

--- src/Entity/Parents.php
+++ src/Entity/Parents.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ParentsRepository")
