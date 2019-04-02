6f34ca1598c4a0a31d80e57dc2fc4600e10cd17f
BaptisteAngot <bat76640@gmail.com>
2019-04-02 15:48:07 +0200

    test get donne by id

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Parents;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
     /**
-     * @Route("/profil", name="profil")
+     * @Route("/profil/{id}", name="profil")
      */
-    public function index()
+    public function index($id)
     {
+
+        $parent = $this->getDoctrine()
+            ->getRepository(Parents::class)
+            ->find($id);
+
+//        return $parent;
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
         ]);
