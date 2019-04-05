1aeb2b3a98d8e7a87e50ba900ad8ea21ac4adb22
BaptisteAngot <bat76640@gmail.com>
2019-04-05 12:30:49 +0200

    WIP Récupération du role

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,33 +2,45 @@
 
 namespace App\Controller;
 
-use App\Entity\Child;
-use App\Entity\Disease;
 use App\Entity\Parents;
 use phpDocumentor\Reflection\Types\This;
-use App\Repository\DiseaseRepository;
 use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil/{id}", name="profil")
      */
-    public function index($id, DiseaseRepository $diseaseRepository)
+    public function index($id, AuthorizationCheckerInterface $authChecker)
     {
-        $parent = $this->getDoctrine()
-            ->getRepository(Parents::class)
-            ->find($id);
 
-        $enfants = $parent->getChildren();
+        if (true === $authChecker->isGranted('ROLE_PARENT'))
+        {
+            $parent = $this->getDoctrine()
+                ->getRepository(Parents::class)
+                ->find($id);
 
-        return $this->render('profilParents/profilParents.html.twig', [
-            'controller_name' => 'Profil',
-            'parents' => $parent,
-            'enfants' => $enfants,
-        ]);
+            $enfants = $parent->getChildren();
+
+            return $this->render('profilParents/profilParents.html.twig', [
+                'controller_name' => 'Profil',
+                'parents' => $parent,
+                'enfants' => $enfants,
+            ]);
+        }
+        elseif(true === $authChecker->isGranted('ROLE_STRUCTURE'))
+        {
+        }
+        else
+        {
+            return $this->render('403/403.html.twig',[
+                'erreur' => 'ACCES FORBIDEN'
+            ]);
+        }
     }
 }
