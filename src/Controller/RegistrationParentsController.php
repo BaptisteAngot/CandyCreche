45461510fad08f0690b536c91cc98b488d22e65e
BaptisteAngot <bat76640@gmail.com>
2019-04-24 14:32:24 +0200

    Envoie de mail lors de l'inscription

--- src/Controller/RegistrationParentsController.php
+++ src/Controller/RegistrationParentsController.php
@@ -15,7 +15,7 @@ class RegistrationParentsController extends AbstractController
     /**
      * @Route("/register_parents", name="app_register_parents")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new Parents();
         $form = $this->createForm(RegistrationsParentsFormType::class, $user);
@@ -35,7 +35,21 @@ class RegistrationParentsController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
-
+            $message  = (new \Swift_Message('Création nouveau compte'))
+                ->setFrom('candycreche1234@gmail.com')
+                ->setTo('candycreche1234@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'mail/mail.html.twig',
+                        [
+                            'mail' => $user->getParentsMail(),
+                            'name' => $user->getParentsName(),
+                            'firstname' => $user->getParentsFirstname(),
+                        ]
+                    ),
+                    'text/html'
+                );
+            $mailer->send($message);
             return $this->redirectToRoute('accueil');
         }
 
