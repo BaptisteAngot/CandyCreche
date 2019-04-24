84f0c3a68fd5c24bac81330d5dd7649c925d8d77
MonSeigneurDEV <tinou.27@hotmail.fr>
2019-04-24 14:26:21 +0200

    changement des designs et labels des formulaires

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -6,15 +6,17 @@ use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('beginAt')
-            ->add('endAt')
-            ->add('title')
+            ->add('beginAt', DateType::class, ['label' => 'Commence le:'])
+            ->add('endAt', DateType::class, ['label' => 'Fini le:'])
+            ->add('title', TextType::class, ['label' => 'Vacances de:'])
         ;
     }
 
