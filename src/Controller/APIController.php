47a23c661b043711f2a341c2bc0073fd18c1c6c5
BaptisteAngot <bat76640@gmail.com>
2019-04-24 11:08:20 +0200

    Sécurisation getinfo

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Child;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -15,26 +16,33 @@ class APIController extends AbstractController
     /**
      * @Route("/authorize/getinfo/{id}",name="getinfo")
      */
-    public function getInfo($id)
+    public function getInfo($id, AuthorizationCheckerInterface $authorizationChecker)
     {
-        $repository = $this->getDoctrine()->getRepository(Child::class);
-        $enfant = $repository->find($id);
+        if (true === $authorizationChecker->isGranted('ROLE_AUTHO')) {
+            $repository = $this->getDoctrine()->getRepository(Child::class);
+            $enfant = $repository->find($id);
 
-        if ($enfant !=null)
-        {
-            return $this->render(
-                'API/index.html.twig',[
-                    'info' => $enfant,
-                ]
-            );
+            if ($enfant !=null)
+            {
+                return $this->render(
+                    'API/index.html.twig',[
+                        'info' => $enfant,
+                    ]
+                );
+            }
+            else
+            {
+                return $this->render(
+                    '403/403.html.twig',[
+                        'erreur' => 'Enfant inexistant'
+                    ]
+                );
+            }
         }
         else
         {
-            return $this->render(
-              '403/403.html.twig',[
-                  'erreur' => 'Enfant inexistant'
-                ]
-            );
+            return $this->redirectToRoute('autho_login');
         }
+
     }
 }
